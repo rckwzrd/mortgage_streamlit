14b945798a2defe582645e947d7fa16425c57857
mlr <mrhoads@gmail.com>
2020-07-31 20:53:35 -0500

    added down payment and main function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# TODO: make branch no_plot, print out down payment amount, add tab for buy and sell, find out way to update down, loan, and home
+# TODO: add tab for buy and sell
 
 # tabs: buy, sell, compare 
 
@@ -23,14 +23,14 @@ def main():
         st.text('Implement Compare') 
 
 def get_input(data:dict):
-    data['home'] = st.number_input('Home Value:', value=270000, step=10000, format='%.2f')
-    data['loan'] = st.number_input('Loan Amount:', value=270000, step=10000, format='%.2f')
-    data['down'] = st.number_input('Downpayment:', value=0.0, step=1000.0, format='%.2f')
+    data['home'] = st.number_input('Home Value:', value=270000.0, step=10000.0, format='%.2f')
+    data['down'] = st.number_input('Downpayment:', value=0.0, step=5000.0, format='%.2f')
+    data['loan'] = st.number_input('Loan Amount:', value=(data['home']-data['down']), step=10000.0, format='%.2f')
     data['rate'] = st.number_input('Interest Rate:', value=0.0325, step=0.0005, format='%.4f')
     data['term'] = int(st.number_input('Term Years:', value=30, format='%i') * 12)
-    data['tax'] = st.number_input('Yearly Tax:',value=6000, format='%.2f') / 12
-    data['hoa'] = st.number_input('Monthly HOA:', value=0, format='%.2f') 
-    data['insurance'] = st.number_input('Yearly Home Insurance:', value=1000, format='%.2f') / 12    
+    data['tax'] = st.number_input('Yearly Tax:',value=6000.0, step=250.0, format='%.2f') / 12
+    data['hoa'] = st.number_input('Monthly HOA:', value=0.0, step=50.0, format='%.2f') 
+    data['insurance'] = st.number_input('Yearly Home Insurance:', value=1000.0, step=250.0, format='%.2f') / 12    
     return data
 
 def total_payment(data:dict):
@@ -41,11 +41,11 @@ def total_payment(data:dict):
 
 def print_output(data:dict):
     st.subheader('Monthly mortgage estimate:')
-    st.text(f"Monthly mortgage rate: {data['rate_periodic']:.4f}, Monthly mortgage payment: {data['periodic_payment']:.2f}")
+    st.text(f"Monthly mortgage payment: ${data['periodic_payment']:.2f}, Monthly mortgage rate: {data['rate_periodic']:.4f}%")
     st.subheader('Monthly escrow estimate:')
-    st.text(f"Tax: {data['tax']:.2f}, HOA: {data['hoa']:.2f}, Home Insurance: {data['insurance']:.2f}")
-    st.subheader(f"Total monthly payment: {data['total_payment']:.2f}")
+    st.text(f"Tax: ${data['tax']:.2f}, HOA: ${data['hoa']:.2f}, Home Insurance: ${data['insurance']:.2f}")
+    st.subheader(f"Total monthly payment: ${data['total_payment']:.2f}")
+    st.text(f"Percent Down: {data['down']/data['home']:.2f}%")
 
 if __name__ == '__main__':
     main()
-
