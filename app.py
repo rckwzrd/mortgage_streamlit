ee9ce0671da78e5e37e93817293c3f8381cff834
mlr <mrhoads@gmail.com>
2020-08-08 08:46:13 -0500

    implemented single page with sidebar

--- app.py
+++ app.py
@@ -1,58 +1,54 @@
 # TODO:
-# make persistent data dict --> declare dict for each tab, merge into main data dict, pass that around
-# make comparison --> need persistent dict
+# convert to single page with side bar, persistent is not working yet --> done
 # make environment and requirements.txt, git ignore .env --> done
+# implement comparison, need current mortgage --> done
+
+# refractor functions to helpers.py
+# annotate main page with markdown
+# implement check boxes
+# implement downpayment, interest plots, and long term modeling
 # deploy to server 
 
 import streamlit as st
-import SessionState
 import numpy as np
-import pandas as pd
 
 def main():
-    st.sidebar.title('Mortgage Modeler')
-    mode = st.sidebar.selectbox('Select Mode', ['Buy','Sell','Compare','Summary','Introduction'])
-    state = SessionState.get(data=dict())
-    # data = dict() # will this be persistent? --> it is not persistent
+    st.title('Mortgage Modeler')
+    data = dict() 
 
-    if mode == 'Buy':
-        st.title("Buy Home")
-        state.data = buy_input(state.data)
-        state.data = total_payment(state.data)
-        buy_output(state.data)
-        st.write(state.data)
+    st.sidebar.subheader("Buy Home")
+    data = buy_input(data)
+    data = total_payment(data)
+    buy_output(data)
     
-    elif mode == 'Sell':
-        st.title('Sell Home')
-        data = sell_input(data)
-        data = sell_cost(data)
-        sell_output(data)
-        st.write(data)
-    elif mode == 'Compare':
-        st.text('Implement Compare') 
-    elif mode == 'Summary':
-        st.text('Implement Summary')
-    elif mode == 'Introduction':
-        st.text('Implement Intro') 
+    st.sidebar.subheader('Sell Home')
+    data = sell_input(data)
+    data = sell_cost(data)
+    sell_output(data)
 
-# @st.cache(persist=True, allow_output_mutation=True, suppress_st_warning=True)
+    st.sidebar.subheader('Compare')
+    data = comparison(data)
+    compare_output(data)
+
+    st.text('Implement downpayment plot') 
+    st.write(data)
+
 def buy_input(data:dict):
-    state.data['home'] = st.sidebar.number_input('Home Value:', value=270000.0, step=10000.0, format='%.2f')
-    state.data['down'] = st.sidebar.number_input('Downpayment:', value=0.0, step=5000.0, format='%.2f')
-    state.data['loan'] = st.sidebar.number_input('Loan Amount:', value=(data['home']-data['down']), step=10000.0, format='%.2f')
-    state.data['rate'] = st.sidebar.number_input('Interest Rate:', value=0.0325, step=0.0005, format='%.4f')
-    state.data['tax'] = st.sidebar.number_input('Yearly Tax:',value=6000.0, step=250.0, format='%.2f') / 12
-    state.data['hoa'] = st.sidebar.number_input('Monthly HOA:', value=0.0, step=50.0, format='%.2f') 
-    state.data['insurance'] = st.sidebar.number_input('Yearly Home Insurance:', value=1000.0, step=250.0, format='%.2f') / 12    
-    return state.data
+    data['home'] = st.sidebar.number_input('Home Value:', value=270000.0, step=10000.0, format='%.2f')
+    data['down'] = st.sidebar.number_input('Downpayment:', value=0.0, step=5000.0, format='%.2f')
+    data['loan'] = st.sidebar.number_input('Loan Amount:', value=(data['home']-data['down']), step=10000.0, format='%.2f')
+    data['rate'] = st.sidebar.number_input('Interest Rate:', value=0.0325, step=0.0005, format='%.4f')
+    data['tax'] = st.sidebar.number_input('Yearly Tax:',value=6000.0, step=250.0, format='%.2f') / 12
+    data['hoa'] = st.sidebar.number_input('Monthly HOA:', value=0.0, step=50.0, format='%.2f') 
+    data['insurance'] = st.sidebar.number_input('Yearly Home Insurance:', value=1000.0, step=250.0, format='%.2f') / 12 
+    return data
 
-# @st.cache(persist=True, allow_output_mutation=True, suppress_st_warning=True)
 def total_payment(data:dict):
-    state.data['term'] = int(30 * 12)
-    state.data['rate_periodic'] = (1+data['rate'])**(1/12) - 1
-    state.data['periodic_payment'] = -1*np.pmt(data['rate_periodic'], data['term'], data['loan'])
-    state.data['total_payment'] = data['periodic_payment'] + data['hoa'] + data['tax'] + data['insurance']
-    return state.data 
+    data['term'] = int(30 * 12)
+    data['rate_periodic'] = (1+data['rate'])**(1/12) - 1
+    data['periodic_payment'] = -1*np.pmt(data['rate_periodic'], data['term'], data['loan'])
+    data['total_payment'] = data['periodic_payment'] + data['hoa'] + data['tax'] + data['insurance']   
+    return data 
 
 def buy_output(data:dict):
     st.subheader(f"Estimated monthly mortgage : ${data['periodic_payment']:.2f}")
@@ -60,10 +56,11 @@ def buy_output(data:dict):
     st.subheader(f"Estimated monthly escrow: ${data['hoa'] + data['tax'] + data['insurance']:.2f}")
     st.text(f"Tax: ${data['tax']:.2f}, HOA: ${data['hoa']:.2f}, Home Insurance: ${data['insurance']:.2f}")
     st.subheader(f"Total monthly payment: ${data['total_payment']:.2f}")
-    st.text(f"Percent Down: {data['down']:.2f}%, {int(data['term']/12)} year term")
+    st.text(f"Percent Down: {(data['down']/data['home']):.2f}%, {int(data['term']/12)} year term")
 
 def sell_input(data:dict):
     data['price'] = st.sidebar.number_input('Home Value:', value=360000.0, step=5000.0, format='%.2f')
+    data['current_payment'] = st.sidebar.number_input('Current Mortgage:', value=2300.0, step=100.0, format='%.2f')
     data['remainder'] = st.sidebar.number_input('Mortgage Remainder:', value=320000.0, step=5000.0, format='%.2f')
     data['repairs'] = st.sidebar.number_input('Repair Cost:', value=2500.0, step=250.0, format='%.2f') 
     data['title'] = st.sidebar.number_input('Title Cost:', value=1000.0, step=250.0, format='%.2f')
@@ -83,14 +80,15 @@ def sell_output(data:dict):
     st.subheader(f"Estimated net profit after sale: ${data['net_profit']:.2f}")
     st.text(f"Sale Price: ${data['price']:.2f}, Profit: ${data['profit']:.2f}") 
 
-def compare_input(data:dict):
-    pass
-
 def comparison(data:dict):
-    pass
+    data['difference'] = data['total_payment'] - data['current_payment']
+    return data
 
 def compare_output(data:dict):
-    pass
+    st.subheader(f"Compare current and estimated mortgage:")
+    st.text(f"Current mortgage: ${data['current_payment']:.2f}")
+    st.text(f"Estimated mortgage: ${data['total_payment']:.2f}")
+    st.text(f"Difference: ${data['difference']:.2f}")
 
 if __name__ == '__main__':
     main()
