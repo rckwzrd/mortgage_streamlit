ea4fb9d0f87fc87a95ddbe3d5dd3df682848db19
MLR <mrhoads1@gmail.com>
2021-02-08 11:41:09 -0600

    switched to plotly and removed extra files

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
 import streamlit as st
-import helper
-import explore_monthly_payment
+import detailed_costs
+import monthly_payments
 
 def main():
     st.title('Mortgage Modeler')
     data = dict()
 
-    mode = st.sidebar.selectbox("Select Mode", ["Detailed Buy/Sell", "Explore Monthly Payment"])
-    if mode == "Detailed Buy/Sell":
-        st.sidebar.subheader("Detailed Buy Home")
-        data = helper.buy_input(data)
-        data = helper.total_payment(data)
-        helper.buy_output(data)
+    mode = st.sidebar.selectbox("Select Mode", ["Start", "Detailed Costs", "Monthly Payments"])
+    
+    if mode == "Start":
+        st.subheader("Print some info about app usage")
+    
+    elif mode == "Detailed Costs":
+        st.sidebar.subheader("Buy Home")
+        data = detailed_costs.buy_input(data)
+        data = detailed_costs.total_payment(data)
+        detailed_costs.buy_output(data)
         
         st.sidebar.subheader('Sell Home')
-        data = helper.sell_input(data)
-        data = helper.sell_cost(data)
-        helper.sell_output(data)
+        data = detailed_costs.sell_input(data)
+        data = detailed_costs.sell_cost(data)
+        detailed_costs.sell_output(data)
 
-        data = helper.comparison(data)
-        helper.compare_output(data)
+        data = detailed_costs.comparison(data)
+        detailed_costs.compare_output(data)
 
-        st.write(data)
-
-    elif mode == "Explore Monthly Payment":
+    elif mode == "Monthly Payments":
         st.sidebar.subheader("Explore Monthly Payment")
-        data = explore_monthly_payment.buy_input(data)
-        st.subheader(f"Home price = ${data['home']:.2f}")
-        data = explore_monthly_payment.model_monthly_payments(data)
-        explore_monthly_payment.plot_payments(data)
+        data = monthly_payments.buy_input(data)
+        data = monthly_payments.model_payments(data)
+        monthly_payments.plot_payments(data)
 
         st.write(data)
+        st.write(data['payments'])
+
 
 
 if __name__ == '__main__':
